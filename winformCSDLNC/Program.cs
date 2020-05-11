1b03bedcadb36c62b6b87c98fbeb6cf4e74f877b
Tran Dat <tranmanhdathsgs@gmail.com>
2020-05-11 07:15:54 +0700

    change to main

--- winformCSDLNC/Program.cs
+++ winformCSDLNC/Program.cs
@@ -16,7 +16,7 @@ namespace winformCSDLNC
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmMain());
         }
     }
 }
